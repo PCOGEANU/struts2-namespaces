84324ceec20e894806dd18263f54c9b1f79f43ea
Paula <pogany2003@yahoo.com>
2015-10-01 22:17:52 +0200

    added struts2 actions, struts.xml, jsp

---
+++ src/org/paula/looks/LookAction.java
@@ -0,0 +1,8 @@
+package org.paula.looks;
+
+public class LookAction {
+	public String execute(){
+		System.out.println("Hello from Look execute");
+		return "error";
+	}
+}
---
+++ src/org/paula/voyages/VoyageAction.java
@@ -0,0 +1,8 @@
+package org.paula.voyages;
+
+public class VoyageAction {
+	public String execute(){
+		System.out.println("Hello VoYAGE execute !!!!");
+		return "success";
+	}
+}
